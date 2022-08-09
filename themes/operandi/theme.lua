f16fd86eb1bdb266e4794db6116a923b4c5d8886
krishna <krishna404@yandex.com>
2022-08-09 12:11:49 +0530

    Added modus operandi themes

---
+++ themes/operandi/theme.lua
@@ -0,0 +1,188 @@
+
+--  ██████╗ ██████╗ ███████╗██████╗  █████╗ ███╗   ██╗██████╗ ██╗
+-- ██╔═══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗████╗  ██║██╔══██╗██║
+-- ██║   ██║██████╔╝█████╗  ██████╔╝███████║██╔██╗ ██║██║  ██║██║
+-- ██║   ██║██╔═══╝ ██╔══╝  ██╔══██╗██╔══██║██║╚██╗██║██║  ██║██║
+-- ╚██████╔╝██║     ███████╗██║  ██║██║  ██║██║ ╚████║██████╔╝██║
+--  ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝
+ 
+local gears = require("gears")
+local lain  = require("lain")
+local awful = require("awful")
+local wibox = require("wibox")
+local dpi   = require("beautiful.xresources").apply_dpi
+
+local os = os
+local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
+
+local wallpaper_dir = "/mnt/extras/Wallpapers"
+
+local theme                                     = {}
+theme.wallpaper                                 = wallpaper_dir .. "/arch_operandi.png"
+
+theme.font                                      = "Iosevka Comfy 11"
+theme.notification_font                         = "Iosevka 19"
+theme.notification_max_width                    = 400
+theme.taglist_font                              = theme.font
+
+theme.white                                     = "#000000"
+theme.dark                                      = "#ffffff"
+theme.darker                                    = "#f8f8f8"
+
+theme.black                                     = theme.dark
+theme.grey_dark                                 = "#505050"
+theme.grey                                      = "#b7b7b7"
+theme.grey_bright                               = "#e0e0e0"
+theme.red                                       = "#a60000"
+theme.orange                                    = "#f2b0a2"
+theme.green                                     = "#005e00"
+theme.green_bright                              = "#145c33"
+theme.yellow                                    = "#813e00"
+theme.blue                                      = "#0031a9"
+theme.blue_bright                               = "#0000c0"
+theme.purple                                    = "#721045"
+theme.purple_bright                             = "#8f0075"
+
+theme.accent                                    = theme.green
+
+theme.fg_normal                                 = theme.grey
+theme.fg_focus                                  = theme.accent
+theme.bg_normal                                 = theme.dark
+theme.bg_focus                                  = theme.dark
+theme.fg_urgent                                 = theme.red
+theme.bg_urgent                                 = theme.dark
+theme.border_width                              = dpi(1)
+theme.border_normal                             = theme.dark
+theme.border_focus                              = theme.green_bright
+
+theme.taglist_fg_focus                          = theme.white
+theme.taglist_bg_focus                          = theme.grey_bright
+theme.taglist_fg_occupied                       = theme.grey_dark
+theme.taglist_fg_empty                          = theme.grey
+theme.taglist_fg_urgent                         = theme.red
+
+theme.bg_systray                                = theme.dark
+theme.menu_height                               = dpi(14)
+theme.menu_width                                = dpi(130)
+
+theme.tasklist_plain_task_name                  = true
+theme.tasklist_disable_icon                     = true
+theme.useless_gap                               = dpi(4)
+
+
+-- awful.util.tagnames   = { "𝟏", "𝟐", "𝟑", "𝟒", "𝟓", "𝟔"}
+awful.util.tagnames   = { "1", "2", "3", "4", "5", "6", "7" }
+-- awful.util.tagnames   = { "ℂ", "ℝ", "ℚ", "ℤ", "ℕ"}
+
+local markup     = lain.util.markup
+local separators = lain.util.separators
+
+-- local clockicon = wibox.widget.imagebox(theme.widget_clock)
+local clock = awful.widget.watch(
+    "date +' %d %b %a %I:%M '", 10,
+    function(widget, stdout)
+        widget:set_markup(" " .. markup.font(theme.font, markup(theme.white, stdout)))
+    end
+)
+
+-- Wifi
+-- local wifi = wibox.widget { nil, neticon, net.widget, layout = wibox.layout.align.horizontal }
+
+-- Battery
+local bat = lain.widget.bat({
+    settings = function()
+        if bat_now.ac_status == 1 then
+            bat_header = " ↑ "
+            -- bat_header = " "
+        else
+            bat_header = " "
+        end
+        bat_p      = " " .. bat_now.perc
+        widget:set_markup(markup.font(theme.font, markup(theme.white, bat_p .. bat_header)))
+    end
+})
+
+-- ALSA volume
+theme.volume = lain.widget.alsa({
+    --togglechannel = "IEC958,3",
+    settings = function()
+        header = "   "
+        vlevel  = volume_now.level
+
+        if volume_now.status == "off" then
+            vlevel = vlevel .. "M"
+        else
+            vlevel = vlevel .. ""
+        end
+
+        widget:set_markup(markup.font(theme.font, markup(theme.white, header .. vlevel)))
+    end
+})
+
+-- Separators
+local first     = wibox.widget.textbox('<span font="Iosevka Comfy 4"> </span>')
+
+local barheight = dpi(24)
+local barcolor  = theme.dark
+
+theme.titlebar_bg = barcolor
+
+local taglist_buttons = gears.table.join(
+                    awful.button({ }, 1, function(t) t:view_only() end),
+                    awful.button({ modkey }, 1, function(t)
+                                              if client.focus then
+                                                  client.focus:move_to_tag(t)
+                                              end
+                                          end),
+                    awful.button({ }, 3, awful.tag.viewtoggle),
+                    awful.button({ modkey }, 3, function(t)
+                                              if client.focus then
+                                                  client.focus:toggle_tag(t)
+                                              end
+                                          end),
+                    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
+                    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
+                )
+
+function theme.at_screen_connect(s)
+    -- Quake application
+    s.quake = lain.util.quake({ app = awful.util.terminal })
+
+    -- If wallpaper is a function, call it with the screen
+    local wallpaper = theme.wallpaper
+    if type(wallpaper) == "function" then
+        wallpaper = wallpaper(s)
+    end
+    gears.wallpaper.maximized(wallpaper, s, true)
+
+    -- Tags
+    awful.tag(awful.util.tagnames, s, awful.layout.layouts[1])
+
+    -- Create a promptbox for each screen
+    s.mypromptbox = awful.widget.prompt()
+    -- Create a taglist widget
+    s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, taglist_buttons)
+
+    -- Create the wibox
+    s.mywibox = awful.wibar({ position = "bottom", screen = s, height = barheight, bg = barcolor})
+
+    -- Add widgets to the wibox
+    s.mywibox:setup {
+        layout = wibox.layout.align.horizontal,
+        { -- Left widgets
+            layout = wibox.layout.fixed.horizontal,
+            s.mytaglist,
+            first,
+        },
+        nil,
+        { -- Right widgets
+            layout = wibox.layout.fixed.horizontal,
+            wibox.widget.systray(),
+            wibox.container.margin(theme.volume.widget, dpi(6), dpi(9)),
+            wibox.container.margin(wibox.widget { bat, layout = wibox.layout.align.horizontal }, dpi(3), dpi(6)),
+            clock,
+	},
+    }
+end
+
+return theme
