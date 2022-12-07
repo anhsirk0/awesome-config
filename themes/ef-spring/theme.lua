df328946ee026c05e1427d40cc815c021961f55a
krishna <krishna404@yandex.com>
2022-12-07 12:22:28 +0530

    Updated variable name

--- themes/ef-spring/theme.lua
+++ themes/ef-spring/theme.lua
@@ -15,7 +15,7 @@ local dpi   = require("beautiful.xresources").apply_dpi
 local os = os
 local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
 
-local wallpaper_dir = "/mnt/extras/Wallpapers"
+local wallpaper_dir = "/mnt/extras/Wallpapers/"
 
 local theme                                     = {}
 theme.wallpaper                                 = wallpaper_dir .. "/spring.jpg"
