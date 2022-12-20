c2c3ce24bc82d1f90beb99bd6d615b5573b2da6a
krishna <krishna404@yandex.com>
2022-12-20 23:57:44 +0530

    Updated change-theme

--- change-theme.pl
+++ change-theme.pl
@@ -1,17 +1,21 @@
 #! /usr/bin/env perl
 
-my $dir = $ENV{HOME} . "/.config/awesome";
-my $config = $ENV{HOME} . "/.config/awesome/rc.lua";
+my $dir    = $ENV{HOME} . "/.config/awesome";
+my $config = $ENV{HOME} . "/.config/awesome/main.lua";
+## use this if you want to use old themes
+# my $config = $ENV{HOME} . "/.config/awesome/rc.lua";
 my $config_content;
 my $theme;
 
 if (scalar @ARGV > 0) {
     my @all_themes = split "\n", `ls $dir/themes`;
+    ## use this if you want to use old themes
+    # my @all_themes = split "\n", `ls $dir/old-themes`;
     ($theme) = grep /$ARGV[0]/, @all_themes;
     if ($theme) {
-	print "\'$theme\' theme selected\n";
+        print "\'$theme\' theme selected\n";
     } else {
-	print "No theme found for \'$ARGV[0]\'\n"; 
+        print "No theme found for \'$ARGV[0]\'\n"; 
     }
 }
 
@@ -23,11 +27,11 @@ unless($theme) {
 if ($theme) {
     open(FH, '<' . $config) or die "Unable to open\n";
     while(<FH>) {
-	if ($_ =~ /^local chosen_theme/) {
-	    $config_content .= "local chosen_theme = \"$theme\"\n";
-	    next;
-	}
-	$config_content .= $_;
+        if ($_ =~ /^local chosen_theme/) {
+            $config_content .= "local chosen_theme = \"$theme\"\n";
+            next;
+        }
+        $config_content .= $_;
     }
     open(FH, '>' . $config) or die "Unable to open\n";
     print FH $config_content;
