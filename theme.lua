16b515f31e088edc1aa001345d662ae0c1c9ef71
krishna <krishna404@yandex.com>
2022-12-22 19:25:45 +0530

    Updated theme.lua

--- theme.lua
+++ theme.lua
@@ -6,7 +6,7 @@ local dpi           = require("beautiful.xresources").apply_dpi
 local bar           = require("bar.bar")
 
 -- For changing themes
-local chosen_theme = "ef-trio-light"
+local chosen_theme = "ef-autumn"
 local colors        = require("themes." .. chosen_theme .. ".colors")
 
 local theme                                     = colors
@@ -16,22 +16,22 @@ theme.taglist_font                              = theme.font
 theme.notification_font                         = "Iosevka 19"
 theme.notification_max_width                    = 400
 
-theme.fg_normal                                 = theme.grey
-theme.fg_focus                                  = theme.accent
-theme.bg_normal                                 = theme.black
-theme.bg_focus                                  = theme.black
-theme.fg_urgent                                 = theme.red
-theme.bg_urgent                                 = theme.black
-theme.border_normal                             = theme.black
-theme.border_focus                              = theme.accent_bright
+-- theme.fg_normal                                 = theme.fg_dim
+-- theme.fg_focus                                  = theme.fb_main
+-- theme.bg_normal                                 = theme.black
+-- theme.bg_focus                                  = theme.black
+-- theme.fg_urgent                                 = theme.red
+-- theme.bg_urgent                                 = theme.black
+theme.border_normal                             = theme.bg_main
+theme.border_focus                              = theme.bg_accent_alt
 
-theme.taglist_fg_focus                          = theme.white
-theme.taglist_bg_focus                          = theme.bg_dim
-theme.taglist_fg_occupied                       = theme.grey
-theme.taglist_fg_empty                          = theme.grey_dark
+theme.taglist_fg_focus                          = theme.fg_main
+theme.taglist_bg_focus                          = theme.bg_active
+theme.taglist_fg_occupied                       = theme.fg_dim
+theme.taglist_fg_empty                          = theme.bg_active
 theme.taglist_fg_urgent                         = theme.red
 
-theme.bg_systray                                = theme.black
+theme.bg_systray                                = theme.bg_main
 theme.border_width                              = dpi(1)
 theme.menu_height                               = dpi(14)
 theme.menu_width                                = dpi(130)
@@ -40,14 +40,15 @@ theme.useless_gap                               = dpi(3)
 theme.tasklist_plain_task_name                  = true
 theme.tasklist_disable_icon                     = true
 
-theme.notification_fg                           = theme.white
-theme.notification_bg                           = theme.accent
-theme.notification_border_color                 = theme.accent
+theme.notification_fg                           = theme.fg_accent
+theme.notification_bg                           = theme.bg_accent
+theme.notification_border_color                 = theme.bg_accent
 
-theme.menubar_fg_focus                          = theme.green_bright
-theme.menubar_fg_normal                         = theme.white
-theme.menubar_bg_normal                         = theme.black
+theme.menubar_fg_focus                          = theme.fg_alt
+theme.menubar_fg_normal                         = theme.fg_main
+theme.menubar_bg_normal                         = theme.bg_main
 
+theme.barcolor = theme.bg_main
 awful.util.tagnames   = { "1", "2", "3", "4", "5", "6", "7" }
 
 function theme.at_screen_connect(s)
