61211e48f6885454f383f9ad75ed8d3a76060b79
krishna <krishna404@yandex.com>
2022-12-24 13:14:09 +0530

    Updated theme.lua

--- theme.lua
+++ theme.lua
@@ -1,56 +1,55 @@
 -- Entry point for themes
 
-local gears         = require("gears")
-local awful         = require("awful")
-local dpi           = require("beautiful.xresources").apply_dpi
-local bar           = require("bar.bar")
+local gears                     = require("gears")
+local awful                     = require("awful")
+local dpi                       = require("beautiful.xresources").apply_dpi
+local bar                       = require("bar.bar")
 
 -- For changing themes
-local chosen_theme = "ef-winter"
-local colors        = require("themes." .. chosen_theme .. ".colors")
+local chosen_theme = "ef-spring"
+local colors                    = require("themes." .. chosen_theme .. ".colors")
 
-local theme                                     = colors
-theme.font                                      = "Iosevka Nerd Font 10"
-theme.taglist_font                              = theme.font
+local theme                     = colors
+theme.font                      = "Iosevka Nerd Font 10"
+theme.taglist_font              = theme.font
 
-theme.notification_font                         = "Iosevka Comfy 19"
-theme.notification_max_width                    = 400
+theme.notification_font         = "Iosevka Comfy 19"
+theme.notification_max_width    = 400
 
-theme.fg_normal                                 = theme.fg_dim
-theme.fg_focus                                  = theme.fb_main
-theme.bg_normal                                 = theme.bg_dim
-theme.bg_focus                                  = theme.bg_main
-theme.fg_urgent                                 = theme.red
-theme.bg_urgent                                 = theme.black
-theme.border_normal                             = theme.bg_main
-theme.border_focus                              = theme.bg_accent_alt
+theme.fg_normal                 = theme.fg_dim
+theme.fg_focus                  = theme.fb_main
+theme.bg_normal                 = theme.bg_dim
+theme.bg_focus                  = theme.bg_main
+theme.fg_urgent                 = theme.red
+theme.bg_urgent                 = theme.black
+theme.border_normal             = theme.bg_main
+theme.border_focus              = theme.bg_accent
 
-theme.taglist_fg_focus                          = theme.fg_main
-theme.taglist_bg_focus                          = theme.bg_active
-theme.taglist_fg_occupied                       = theme.fg_dim
-theme.taglist_fg_empty                          = theme.bg_active
-theme.taglist_fg_urgent                         = theme.red
+theme.taglist_fg_focus          = theme.fg_main
+theme.taglist_bg_focus          = theme.bg_active
+theme.taglist_fg_occupied       = theme.fg_dim
+theme.taglist_fg_empty          = theme.bg_active
+theme.taglist_fg_urgent         = theme.red
 
-theme.bg_systray                                = theme.bg_main
-theme.border_width                              = dpi(1)
-theme.menu_height                               = dpi(14)
-theme.menu_width                                = dpi(130)
-theme.useless_gap                               = dpi(3)
+theme.bg_systray                = theme.bg_main
+theme.border_width              = dpi(1)
+theme.useless_gap               = dpi(3)
 
-theme.tasklist_plain_task_name                  = true
-theme.tasklist_disable_icon                     = true
+theme.tasklist_plain_task_name  = true
+theme.tasklist_disable_icon     = true
 
-theme.notification_fg                           = theme.fg_accent
-theme.notification_bg                           = theme.bg_accent
-theme.notification_border_color                 = theme.bg_accent
+theme.notification_fg           = theme.fg_accent_alt
+theme.notification_bg           = theme.bg_accent_alt
+theme.notification_border_color = theme.bg_accent_alt
 
-theme.menubar_fg_focus                          = theme.fg_alt_alt
-theme.menubar_bg_focus                         = theme.bg_accent_alt
-theme.menubar_fg_normal                         = theme.fg_main
-theme.menubar_bg_normal                         = theme.bg_main
+theme.menubar_height            = dpi(22)
+theme.menubar_fg_focus          = theme.fg_alt_alt
+theme.menubar_bg_focus          = theme.bg_accent_alt
+theme.menubar_fg_normal         = theme.fg_main
+theme.menubar_bg_normal         = theme.bg_main
 
-theme.barcolor = theme.bg_main
-awful.util.tagnames   = { "1", "2", "3", "4", "5", "6", "7" }
+theme.barcolor                  = theme.bg_main
+awful.util.tagnames             = { "1", "2", "3", "4", "5", "6", "7" }
 
 function theme.at_screen_connect(s)
 
