bc13c2b9a75fd6ce04013a017d44fe328fa03df4
krishna <krishna404@yandex.com>
2023-05-10 13:55:32 +0530

    Added ef-tritanopia-dark

---
+++ themes/ef-tritanopia-dark/colors.lua
@@ -0,0 +1,35 @@
+-- ┏━╸┏━╸   ╺┳╸┏━┓╻╺┳╸┏━┓┏┓╻┏━┓┏━┓╻┏━┓   ╺┳┓┏━┓┏━┓╻┏ 
+-- ┣╸ ┣╸ ╺━╸ ┃ ┣┳┛┃ ┃ ┣━┫┃┗┫┃ ┃┣━┛┃┣━┫╺━╸ ┃┃┣━┫┣┳┛┣┻┓
+-- ┗━╸╹      ╹ ╹┗╸╹ ╹ ╹ ╹╹ ╹┗━┛╹  ╹╹ ╹   ╺┻┛╹ ╹╹┗╸╹ ╹
+
+local colors          = {}
+
+colors.wallpaper      = "#282026"
+colors.bg_main        = "#15050f"
+colors.fg_main        = "#dfd0d5"
+colors.bg_dim         = "#282026"
+colors.fg_dim         = "#908890"
+colors.bg_alt         = "#352f2f"
+colors.fg_alt         = "#afdaef"
+colors.bg_active      = "#554f4f"
+colors.bg_inactive    = "#1a1517"
+
+colors.red            = "#cf4f5f"
+colors.red_bright     = "#d24f7f"
+colors.green          = "#2fa526"
+colors.green_bright   = "#00b066"
+colors.yellow         = "#c48702"
+colors.yellow_bright  = "#df8f6f"
+colors.blue           = "#379cf6"
+colors.blue_bright    = "#029fff"
+colors.magenta        = "#b2608f"
+colors.magenta_bright = "#a6699f"
+colors.cyan           = "#3fafcf"
+colors.cyan_bright    = "#4fafaf"
+
+colors.bg_accent_alt  = "#671822"
+colors.fg_accent_alt  = "#ffffff"
+colors.bg_accent      = "#7fccee"
+colors.fg_accent      = "#15050f"
+
+return colors
