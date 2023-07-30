3b63533f47ed68203c9e0e644d2ecdb59c1d8bda
krishna <krishna404@yandex.com>
2023-07-30 12:28:30 +0530

    Added modus-themes {deuteranopia, tinted, tritanopia} variants

---
+++ themes/vivendi-deuteranopia/colors.lua
@@ -0,0 +1,28 @@
+-- Colors Modus-Vivendi-Deuteranopia
+
+local colors = {}
+colors.wallpaper     = "#1e1e1e"
+colors.bg_main       = "#000000"
+colors.fg_main       = "#ffffff"
+colors.bg_dim        = "#1e1e1e"
+colors.fg_dim        = "#989898"
+colors.bg_alt        = "#535353"
+colors.fg_alt        = "#c6daff"
+colors.bg_active     = "#535353"
+colors.bg_inactive   = "#303030"
+
+colors.red           = "#ff5f59"
+colors.green         = "#44bc44"
+colors.green_bright  = "#00c06f"
+colors.yellow        = "#cabf00"
+colors.yellow_bright = "#d8af7a"
+colors.blue          = "#2fafff"
+colors.blue_bright   = "#00bcff"
+colors.purple        = "#feacd0"
+colors.purple_bright = "#b6a0ff"
+colors.bg_accent_alt = "#2a2a6a"
+colors.fg_accent_alt = "#ffffff"
+colors.bg_accent     = "#7030af"
+colors.fg_accent     = "#ffffff"
+
+return colors
