ebd2fe4c76935b5644693a087ccee2a788e09e3f
krishna <krishna404@yandex.com>
2023-08-12 17:18:56 +0530

    Added ef-maris themes

---
+++ themes/ef-maris-dark/colors.lua
@@ -0,0 +1,28 @@
+-- Colors Ef-Maris-Dark
+
+local colors = {}
+colors.wallpaper     = "#1d2c39"
+colors.bg_main       = "#131c2b"
+colors.fg_main       = "#eaedef"
+colors.bg_dim        = "#1d2c39"
+colors.fg_dim        = "#969faf"
+colors.bg_alt        = "#2a3644"
+colors.fg_alt        = "#90a0dc"
+colors.bg_active     = "#4a5664"
+colors.bg_inactive   = "#1a2331"
+
+colors.red           = "#ff6f6f"
+colors.green         = "#41bf4f"
+colors.green_bright  = "#30c489"
+colors.yellow        = "#d0d24f"
+colors.yellow_bright = "#cab27f"
+colors.blue          = "#57b0ff"
+colors.blue_bright   = "#12b4ff"
+colors.purple        = "#f59acf"
+colors.purple_bright = "#cf90ff"
+colors.bg_accent_alt = "#2f527b"
+colors.fg_accent_alt = "#ecf0ff"
+colors.bg_accent     = "#8fdfff"
+colors.fg_accent     = "#eaedef"
+
+return colors
---
+++ themes/ef-maris-light/colors.lua
@@ -0,0 +1,28 @@
+-- Colors Ef-Maris-Light
+
+local colors = {}
+colors.wallpaper     = "#e0e7ef"
+colors.bg_main       = "#edf4f8"
+colors.fg_main       = "#151a27"
+colors.bg_dim        = "#e0e7ef"
+colors.fg_dim        = "#676470"
+colors.bg_alt        = "#cfd8e3"
+colors.fg_alt        = "#3f4f99"
+colors.bg_active     = "#afb8c3"
+colors.bg_inactive   = "#e6ecf2"
+
+colors.red           = "#c3303a"
+colors.green         = "#007010"
+colors.green_bright  = "#007047"
+colors.yellow        = "#805a1f"
+colors.yellow_bright = "#78542f"
+colors.blue          = "#375cc6"
+colors.blue_bright   = "#003faf"
+colors.purple        = "#80308f"
+colors.purple_bright = "#5f2fba"
+colors.bg_accent_alt = "#a0c2ef"
+colors.fg_accent_alt = "#142810"
+colors.bg_accent     = "#036f99"
+colors.fg_accent     = "#151a27"
+
+return colors
