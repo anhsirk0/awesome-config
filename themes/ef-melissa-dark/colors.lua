5f5ab4ead5f01a3c5a00908bdce703c7d5ae2518
krishna <krishna404@yandex.com>
2023-10-26 17:57:44 +0530

    Added ef-melissa

---
+++ themes/ef-melissa-dark/colors.lua
@@ -0,0 +1,28 @@
+-- Colors Ef-Melissa-Dark
+
+local colors = {}
+colors.wallpaper     = "#483426"
+colors.bg_main       = "#352718"
+colors.fg_main       = "#e8e4b1"
+colors.bg_dim        = "#483426"
+colors.fg_dim        = "#90918a"
+colors.bg_alt        = "#59463f"
+colors.fg_alt        = "#a2c080"
+colors.bg_active     = "#79665f"
+colors.bg_inactive   = "#3d2e1f"
+
+colors.red           = "#ff7f7f"
+colors.green         = "#6fd560"
+colors.green_bright  = "#65d590"
+colors.yellow        = "#e4b53f"
+colors.yellow_bright = "#e7a06f"
+colors.blue          = "#57aff6"
+colors.blue_bright   = "#62cfef"
+colors.purple        = "#f0aac5"
+colors.purple_bright = "#c6a2fe"
+colors.bg_accent_alt = "#704f00"
+colors.fg_accent_alt = "#f8efd8"
+colors.bg_accent     = "#f9cf7a"
+colors.fg_accent     = "#e8e4b1"
+
+return colors
---
+++ themes/ef-melissa-light/colors.lua
@@ -0,0 +1,28 @@
+-- Colors Ef-Melissa-Light
+
+local colors = {}
+colors.wallpaper     = "#f5e9cb"
+colors.bg_main       = "#fff6d8"
+colors.fg_main       = "#484431"
+colors.bg_dim        = "#f5e9cb"
+colors.fg_dim        = "#68708a"
+colors.bg_alt        = "#e7d7c6"
+colors.fg_alt        = "#66713a"
+colors.bg_active     = "#c7b7a6"
+colors.bg_inactive   = "#f9efd4"
+
+colors.red           = "#ba2d2f"
+colors.green         = "#007a0a"
+colors.green_bright  = "#008250"
+colors.yellow        = "#a46110"
+colors.yellow_bright = "#946830"
+colors.blue          = "#375cc6"
+colors.blue_bright   = "#265fbf"
+colors.purple        = "#aa3e74"
+colors.purple_bright = "#6448ca"
+colors.bg_accent_alt = "#f3cf72"
+colors.fg_accent_alt = "#403328"
+colors.bg_accent     = "#a07f00"
+colors.fg_accent     = "#484431"
+
+return colors
