e77654692b9ea92eb6f9f545009aca190dc742a6
krishna <krishna404@yandex.com>
2024-02-11 19:08:49 +0530

    Add rosa, arbutus

---
+++ themes/ef-arbutus/colors.lua
@@ -0,0 +1,28 @@
+-- Colors Ef-Arbutus
+
+local colors = {}
+colors.wallpaper     = "#f0d8cf"
+colors.bg_main       = "#ffead8"
+colors.fg_main       = "#393330"
+colors.bg_dim        = "#f0d8cf"
+colors.fg_dim        = "#6e678f"
+colors.bg_alt        = "#e7d2cb"
+colors.fg_alt        = "#8a5f4a"
+colors.bg_active     = "#c7b2ab"
+colors.bg_inactive   = "#f7e2d2"
+
+colors.red           = "#b0000f"
+colors.green         = "#007000"
+colors.green_bright  = "#00704f"
+colors.yellow        = "#906200"
+colors.yellow_bright = "#8a6340"
+colors.blue          = "#375cc6"
+colors.blue_bright   = "#265fbf"
+colors.purple        = "#a23ea4"
+colors.purple_bright = "#6448ca"
+colors.bg_accent_alt = "#e9a0a0"
+colors.fg_accent_alt = "#40231f"
+colors.bg_accent     = "#208f10"
+colors.fg_accent     = "#393330"
+
+return colors
---
+++ themes/ef-rosa/colors.lua
@@ -0,0 +1,28 @@
+-- Colors Ef-Rosa
+
+local colors = {}
+colors.wallpaper     = "#432e32"
+colors.bg_main       = "#322023"
+colors.fg_main       = "#e4d3e1"
+colors.bg_dim        = "#432e32"
+colors.fg_dim        = "#9d9d9d"
+colors.bg_alt        = "#4a3842"
+colors.fg_alt        = "#96c4af"
+colors.bg_active     = "#6a5862"
+colors.bg_inactive   = "#362129"
+
+colors.red           = "#ff707f"
+colors.green         = "#55cb56"
+colors.green_bright  = "#49d081"
+colors.yellow        = "#e4c53f"
+colors.yellow_bright = "#eec26f"
+colors.blue          = "#57aff6"
+colors.blue_bright   = "#62cff7"
+colors.purple        = "#ffb2d6"
+colors.purple_bright = "#cfb1ff"
+colors.bg_accent_alt = "#814558"
+colors.fg_accent_alt = "#e8e5e7"
+colors.bg_accent     = "#ef607a"
+colors.fg_accent     = "#e4d3e1"
+
+return colors
